ae5c027ee3ace4d2bd24c1f9a1ca2b062b132c5e
rannysan <bs.raniel@gmail.com>
2018-11-27 12:41:26 -0200

    estruturado jogo da memoria

--- myscrum/src/index.js
+++ myscrum/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AppGames from './AppGames';
+import Memory from './AppMemory';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ ReactDOM.render(
         <Switch>
             <Route path="/" exact={true} component={App} />
             <Route path="/games" component={AppGames} />
+            <Route path="/memory_game" component={Memory} />
         </Switch>
     </ BrowserRouter>
     , document.getElementById('root'));
